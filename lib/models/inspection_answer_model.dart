9858a8544858d7d7e7bbd55fa9dd798f9c0b7c84
nakavandi-cell <nakavandi@gmail.com>
2026-08-15 11:32:23 +0330

    Enhance InspectionAnswer model with additional fields
    Added optional fields for comment and photoPath in InspectionAnswer model.

--- lib/models/inspection_answer_model.dart
+++ lib/models/inspection_answer_model.dart
@@ -2,9 +2,38 @@ class InspectionAnswer {
   final int? id;
   final int inspectionId;
   final int checklistItemId;
-  final String result; // 'Pass', 'Fail'
+  final String result; // 'Pass', 'Fail', 'N/A'
+  final String? comment;
+  final String? photoPath;
 
-  InspectionAnswer({this.id, required this.inspectionId, required this.checklistItemId, required this.result});
+  InspectionAnswer({
+    this.id,
+    required this.inspectionId,
+    required this.checklistItemId,
+    required this.result,
+    this.comment,
+    this.photoPath,
+  });
 
-  Map<String, dynamic> toMap() => {'id': id, 'inspectionId': inspectionId, 'checklistItemId': checklistItemId, 'result': result};
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'inspectionId': inspectionId,
+      'checklistItemId': checklistItemId,
+      'result': result,
+      'comment': comment,
+      'photoPath': photoPath,
+    };
+  }
+
+  factory InspectionAnswer.fromMap(Map<String, dynamic> map) {
+    return InspectionAnswer(
+      id: map['id'] as int?,
+      inspectionId: map['inspectionId'] as int,
+      checklistItemId: map['checklistItemId'] as int,
+      result: map['result'] as String,
+      comment: map['comment'] as String?,
+      photoPath: map['photoPath'] as String?,
+    );
+  }
 }
