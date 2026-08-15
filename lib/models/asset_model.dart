11020d3634b21e309fcfcee05462d0155fff3fa9
nakavandi-cell <nakavandi@gmail.com>
2026-08-15 12:48:07 +0330

    Modify Asset class with new type options and formatting
    Updated asset type options and improved formatting in the Asset class.

--- lib/models/asset_model.dart
+++ lib/models/asset_model.dart
@@ -2,10 +2,34 @@ class Asset {
   final int? id;
   final String assetCode;
   final String name;
-  final String type; // 'Fire', 'Panel', 'Substation'
+  final String type; // 'Extinguisher', 'Electrical Panel', 'Substation'
   final String location;
 
-  Asset({this.id, required this.assetCode, required this.name, required this.type, required this.location});
+  Asset({
+    this.id,
+    required this.assetCode,
+    required this.name,
+    required this.type,
+    required this.location,
+  });
 
-  Map<String, dynamic> toMap() => {'id': id, 'assetCode': assetCode, 'name': name, 'type': type, 'location': location};
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'assetCode': assetCode,
+      'name': name,
+      'type': type,
+      'location': location,
+    };
+  }
+
+  factory Asset.fromMap(Map<String, dynamic> map) {
+    return Asset(
+      id: map['id'] as int?,
+      assetCode: map['assetCode'] as String,
+      name: map['name'] as String,
+      type: map['type'] as String,
+      location: map['location'] as String,
+    );
+  }
 }
