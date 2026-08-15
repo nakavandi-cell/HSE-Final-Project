5702942f6ab760f6600fe5e8c75607e7b63cc2f7
nakavandi-cell <nakavandi@gmail.com>
2026-08-15 11:31:13 +0330

    Refactor ChecklistItem model for clarity
    Refactor ChecklistItem constructor and toMap method for improved readability.

--- lib/models/checklist_item_model.dart
+++ lib/models/checklist_item_model.dart
@@ -3,7 +3,25 @@ class ChecklistItem {
   final int checklistId;
   final String question;
 
-  ChecklistItem({this.id, required this.checklistId, required this.question});
+  ChecklistItem({
+    this.id,
+    required this.checklistId,
+    required this.question,
+  });
 
-  Map<String, dynamic> toMap() => {'id': id, 'checklistId': checklistId, 'question': question};
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'checklistId': checklistId,
+      'question': question,
+    };
+  }
+
+  factory ChecklistItem.fromMap(Map<String, dynamic> map) {
+    return ChecklistItem(
+      id: map['id'] as int?,
+      checklistId: map['checklistId'] as int,
+      question: map['question'] as String,
+    );
+  }
 }
